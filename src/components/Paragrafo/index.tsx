c7d400fc33b265bd4eb3fc900b410a5371ae159e
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2025-05-17 21:23:51 -0300

    estilizando a sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -4,10 +4,13 @@ import { P } from './style'
 export type Props = {
   children: ReactNode // Agora pode aceitar múltiplos elementos
   tipo?: 'principal' | 'secundario'
+  fontSize?: number
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <P tipo={tipo}>{children}</P>
+const Paragrafo = ({ children, tipo = 'principal', fontSize }: Props) => (
+  <P fontSize={fontSize} tipo={tipo}>
+    {children}
+  </P>
 )
 
 export default Paragrafo
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import { Avatar } from '../../components/Avatar/indesx'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/sidneygyne.png" alt="" />
-    <Titulo fontSize={20}>Sidney</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Sidney</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        sidneygyne
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
